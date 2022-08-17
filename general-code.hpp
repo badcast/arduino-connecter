89e475bc3f488519c56e135dc2c8bc730a64e84b
badcast <70087152+badcast@users.noreply.github.com>
2022-08-18 02:11:26 +0600

    Added malloc request, and combined ard/os

--- general-code.hpp
+++ general-code.hpp
@@ -4,14 +4,27 @@
 #define function
 
 #ifdef __unix__ || __MSVC__ || __WINRT__  // linux/unix/FreeDOS code
+
 #include <unistd.h>
-namespace connector {
 int millis();
+#define ARG_FD_DEF int fd,
+#define ARG_FD_SIN int fd
+#define ARG_FD_USE fd,
+
+#define SEND(BUFFER, LENGTH) write(fd, BUFFER, LENGTH)
+#define READ(BUFFER, LENGTH) read(fd, BUFFER, LENGTH)
+
 #else  // arduino code
-#define _USE_PRINT
+#include <LiquidCrystal_I2C.h>  // подключаем библиотеку для QAPASS 1602
+#define ARG_FD_DEF 
+#define ARG_FD_SIN
+#define ARG_FD_USE 
+//#define USE_PRINT
+
 #define SEND(BUFFER, LENGTH) Serial.write(reinterpret_cast<const char*>(BUFFER), LENGTH)
 #define READ(BUFFER, LENGTH) Serial.readBytes(reinterpret_cast<char*>(BUFFER), LENGTH)
 extern LiquidCrystal_I2C lcd(0x27, 16, 2);  // присваиваем имя LCD для дисплея
+
 #endif
 
 const int default_timeout = 1000;
@@ -22,13 +35,16 @@ const uint8_t cmd_reset = 0xff;  // 255
 const uint8_t cmd_quit = 0xfe;   // 254
 const uint8_t cmd_echo = 0xfd;   // echo command for IO
 
+typedef uint16_t dptr;
+
 enum CommandMethod : uint8_t { GET, SET };
 
 enum GeneralCommand : uint8_t {
     // Command a work lcd text
     LCD_TEXT = 2,
     LCD_BACKLIGHT_MODE = 4,
-    LCD_CURSOR_POS = 6
+    LCD_CURSOR_POS = 6,
+    DEVICE_MALLOC = 8
     // next 8, 10, 12, 14
     // TODO: do next command's
 };
@@ -43,11 +59,11 @@ struct command_request : command_queue {};
 
 struct command_responce {};
 
-struct RequestStatus : command_request {
-    RequestStatus() { commandFlag = LowLevelCommand::GET_STATUS_INIT; }
+struct RequestInitStatus : command_request {
+    RequestInitStatus() { commandFlag = LowLevelCommand::GET_STATUS_INIT; }
 };
 
-struct ResponceStatus : command_responce {
+struct ResponceInitStatus : command_responce {
     uint16_t memsize;
     uint16_t memfree;
     uint16_t lcd_data;
@@ -70,67 +86,78 @@ struct ResponceBacklight : command_responce {
 
 struct RequestText : command_request {
     uint8_t _length;
-    char* buffer;
+    const char* buffer;
     // get
-    RequestText() {
-        this->length = 0;
-        this->commandFlag = GeneralCommand::LCD_TEXT;
-    }
+    RequestText() { this->commandFlag = GeneralCommand::LCD_TEXT; }
     // set
-    RequestText(char* buffer, uint8_t length) : RequestText(), _length(length) {
+    RequestText(const char* buffer, uint8_t length) : RequestText() {
+        _length = length;
         this->buffer = buffer;
         this->commandFlag |= SET;
     }
 };
 
+struct RequestFreeMemory : command_request {
+  dptr pointer;
+};
+
+struct RequestMalloc : command_request {
+    uint8_t requestSize;
+    RequestMalloc(uint8_t size) : requestSize(size) { commandFlag = DEVICE_MALLOC; }
+};
+
+struct ResponceMalloc : command_responce {
+    dptr pointer;
+};
+
+function int read_timeout(ARG_FD_DEF void* buffer, const uint16_t& len, int timeout = default_timeout);
+function uint16_t send(ARG_FD_DEF const void* buffer, const uint16_t& len);
+function bool echo_input(ARG_FD_SIN);
+function bool echo_output(ARG_FD_SIN);
+function bool echo_test(ARG_FD_SIN);
+
+inline bool echo_input(ARG_FD_SIN) {
+    uint8_t echo;
+    return read_timeout(ARG_FD_USE & echo, 1, echo_timeout) && echo == cmd_echo;
+}
+
+inline bool echo_output(ARG_FD_SIN) { return send(ARG_FD_USE & cmd_echo, 1); }
+
+// state is connected from "echo"
+bool echo_test(ARG_FD_SIN) {
 #ifdef ARDUINO
-function int read_timeout(void* buffer, const uint16_t& len, int timeout = default_timeout);
-function uint16_t send(const void* buffer, const uint16_t& len);
-function bool echo_input();
-function bool echo_output();
-function bool echo_test();
+    return echo_output() && echo_input();
 #else
-function int read_timeout(int fd, void* buffer, const uint16_t& len, int timeout = default_timeout);
-function uint16_t send(int fd, const void* buffer, const uint16_t& len);
-function bool echo_input(int fd);
-function bool echo_output(int fd);
-function bool echo_test(int fd);
+    return echo_input(fd) && echo_output(fd);
 #endif
+}
 
-#ifdef !ARDUINO
-
-int read_timeout(void* buffer, const uint16_t& len, int timeout = default_timeout) {
+int read_timeout(ARG_FD_DEF void* buffer, const uint16_t& len, int timeout) {
     uint8_t alpha;
     int nreaded = 0;
     timeout = millis() + timeout;
     do {
         // read left
         alpha = READ((reinterpret_cast<int8_t*>(buffer) + nreaded), len - nreaded);
+#ifndef ARDUINO
+        if (alpha == -1) alpha = 0;
+#endif
         nreaded += alpha;
     } while (timeout > millis() && nreaded < len);
     return nreaded;
 }
 
-uint16_t send(const void* buffer, const uint16_t& len) {
+uint16_t send(ARG_FD_DEF const void* buffer, const uint16_t& len) {
     uint16_t alpha = SEND(buffer, len);
+#ifdef ARDUINO
     Serial.flush();  // sync
+#else
+    tcdrain(fd);  // sync
+#endif
     return alpha;
 }
 
-inline bool echo_input() {
-    uint8_t echo;
-    return read_timeout(&echo, 1, echo_timeout) && echo == cmd_echo;
-}
-
-inline bool echo_output() { return send(&cmd_echo, 1); }
-
-// state is connected from "echo"
-
-bool echo_test() {
-    // Arduino first send and reply
-    return echo_output() && echo_input();
-}
-
+#ifdef ARDUINO
 extern int __heap_start, *__brkval;
 inline uint16_t heapstart() { return (uint16_t)(__brkval == 0 ? (uint16_t)&__heap_start : (uint16_t)__brkval); }
 uint16_t freeRam() {
@@ -167,6 +194,12 @@ void arduino_reply(command_request* request) {
             lcd.clear();
 #endif
             switch (static_cast<GeneralCommand>(request->commandFlag)) {
+// *** RequestMalloc *** *** *** *** *** *** *** ***
+#define wrapper (reinterpret_cast<ResponceMalloc*>(pokets))
+                case GeneralCommand::DEVICE_MALLOC:
+                    pokets = ::malloc(sizeof(*wrapper));
+                    wrapper->pointer = reinterpret_cast<dptr>(::malloc((reinterpret_cast<RequestMalloc*>(request))->requestSize));
+                    break;
 // *** RequestBacklight *** *** *** *** *** *** *** ***
 #define wrapper (reinterpret_cast<ResponceBacklight*>(pokets))
                 case GeneralCommand::LCD_BACKLIGHT_MODE:
@@ -184,8 +217,7 @@ void arduino_reply(command_request* request) {
                             break;
                     }
                     break;
-
-                    // *** RequestText *** *** *** *** *** *** *** ***
+// *** RequestText *** *** *** *** *** *** *** ***
 #define wrapper (reinterpret_cast<ResponceBacklight*>(pokets))
                 case GeneralCommand::LCD_TEXT:
                     switch (cmdMethod) {
@@ -206,10 +238,5 @@ void arduino_reply(command_request* request) {
         free(pokets);  // free
     }
 }
-#endif
-
 #undef wrapper
-
-#ifndef ARDUINO
-}  // namespace connector
 #endif
