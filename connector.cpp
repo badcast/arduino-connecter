e34da7a31b8c9677ec6655783a092a6c90513116
Bad Cast <lmecomposer@gmail.com>
2023-06-01 15:32:33 +0600

    Disconnect of the connected

--- connector.cpp
+++ connector.cpp
@@ -21,16 +21,24 @@ constexpr char determ =
 
 using namespace connector;
 
-Port::Port() : Port(PortID::port0) {}
+Port::Port()
+    : Port(PortID::port0)
+{
+}
 
-Port::Port(PortID port) : _id(port) {}
+Port::Port(PortID port)
+    : _id(port)
+{
+}
 
-std::string Port::name() {
+std::string Port::name()
+{
     std::string _str = fullname();
     return _str.c_str() + _str.find_last_of(determ) + 1;
 }
 
-std::string Port::fullname() {
+std::string Port::fullname()
+{
     char portname[64];
     sprintf(portname, port_place_holder, (int)_id);
     return portname;
@@ -40,36 +48,40 @@ const PortID Port::getPortID() { return _id; }
 
 void delay(int ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }
 
-int millis() {
+int millis()
+{
     using namespace std::chrono;
     uint64_t tms = (duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count());
     static uint64_t tcapture = tms;
     return static_cast<int>(tms - tcapture);
 }
 
-int command_write(int fd, const void* buffer, int sz) {
+int command_write(int fd, const void* buffer, int sz)
+{
     // write attribute for indentification
     int nwrit = send(fd, buffer, sz);
-    tcdrain(fd);  // sync
+    tcdrain(fd); // sync
     return nwrit;
 }
 
-bool device_awake(Connector& con, const command_request* requestCmd, command_responce* responceCmd, uint8_t size_req,
-                  uint8_t size_resp, const char* buffer = nullptr, int size = -1) {
+bool device_awake(Connector& con, const command_request* requestCmd, command_responce* responceCmd, uint8_t size_req, uint8_t size_resp, const char* buffer = nullptr, int size = -1)
+{
     int alpha;
 
     if (!con.isConnected()) {
         throw std::runtime_error("Device is not connected");
     }
 
-    if (requestCmd == nullptr) throw std::runtime_error("requestCmd is null");
+    if (requestCmd == nullptr)
+        throw std::runtime_error("requestCmd is null");
 
     if ((requestCmd->commandFlag & 0x1) == SET) {
-        if (responceCmd) throw std::runtime_error("request method not requirable responce arg");
+        if (responceCmd)
+            throw std::runtime_error("request method not requirable responce arg");
     } else if (responceCmd == nullptr)
         throw std::runtime_error("responceCmd is null");
 
-    tcflush(con.handle(), TCIOFLUSH);  // flushing
+    tcflush(con.handle(), TCIOFLUSH); // flushing
 
     // send content size
     alpha = send(con.handle(), &size_req, 1);
@@ -79,13 +91,14 @@ bool device_awake(Connector& con, const command_request* requestCmd, command_res
         return false;
     }
 
-    delay(10);  // custom delay
+    delay(10); // custom delay
 
     // send request
     alpha = send(con.handle(), requestCmd, size_req);
     // send buffer
     if (buffer) {
-        if (size == -1) size = strlen(buffer);
+        if (size == -1)
+            size = strlen(buffer);
         uint8_t* q = (uint8_t*)malloc(1);
         alpha = read_timeout(con.handle(), q, 1);
         free(q);
@@ -98,33 +111,39 @@ bool device_awake(Connector& con, const command_request* requestCmd, command_res
     }
 
     alpha = read_timeout(con.handle(), responceCmd, size_resp);
-    if (!(alpha == size_resp)) return false;
+    if (!(alpha == size_resp))
+        return false;
 
     return true;
 }
 
 template <typename T1, typename T2>
-bool Connector::get(const T1& request, T2& responce) {
+bool Connector::get(const T1& request, T2& responce)
+{
     return device_awake(*this, &request, &responce, sizeof request, sizeof responce);
 }
 
 template <typename Request>
-bool Connector::set(const Request& req) {
+bool Connector::set(const Request& req)
+{
     return device_awake(*this, &req, nullptr, sizeof req, 0);
 }
 
 template <typename Request>
-bool Connector::set(const Request& request, const char* buffer) {
+bool Connector::set(const Request& request, const char* buffer)
+{
     return device_awake(*this, &request, nullptr, sizeof request, 0, buffer);
 }
 
-void Connector::reset() {
+void Connector::reset()
+{
     uint8_t cmdReset = cmd_reset;
     write(_fd, &cmdReset, 1);
     tcdrain(_fd);
 }
 
-bool Connector::init() {
+bool Connector::init()
+{
     uint8_t attrib;
     if (!read_timeout(_fd, &attrib, 1, 3000)) {
         return false;
@@ -139,8 +158,11 @@ bool Connector::init() {
 
 Connector::Connector() { _fd = ~0; }
 
-Connector::Connector(const Port& port) : __port(port) {
-    if (!connect(__port)) throw std::runtime_error("connection failed");
+Connector::Connector(const Port& port)
+    : __port(port)
+{
+    if (!connect(__port))
+        throw std::runtime_error("connection failed");
 }
 
 const int Connector::handle() const { return _fd; }
@@ -149,7 +171,8 @@ bool Connector::isConnected() const { return _fd != ~0; }
 
 Port Connector::port() { return __port; }
 
-bool Connector::connect() {
+bool Connector::connect()
+{
     // auto connect
     bool status;
     // start find from 0
@@ -160,12 +183,18 @@ bool Connector::connect() {
     return status;
 }
 
-bool Connector::connect(Port port) {
+bool Connector::connect(Port port)
+{
     int alpha;
     bool status;
+
+    if (_fd != ~0) {
+        close(_fd);
+    }
+
     _fd = open(port.fullname().c_str(), O_RDWR);
     while (status = _fd != -1) {
-        tty = {0};
+        tty = { 0 };
 
         // error handling
         alpha = tcgetattr(_fd, &tty);
@@ -178,20 +207,20 @@ bool Connector::connect(Port port) {
         cfsetspeed(&tty, B9600);
 
         // Setting other Port Stuff
-        tty.c_cflag &= ~PARENB;  // Make 8n1
+        tty.c_cflag &= ~PARENB; // Make 8n1
         tty.c_cflag &= ~CSTOPB;
         tty.c_cflag &= ~CSIZE;
         tty.c_cflag |= CS8;
-        tty.c_cflag &= ~CRTSCTS;  // no flow control
-        tty.c_lflag = 0;          // no signaling chars, no echo, no canonical processing
-        tty.c_oflag = 0;          // no remapping, no delays
-        tty.c_cc[VMIN] = 0;       // read doesn't block
-        tty.c_cc[VTIME] = 5;      // 0.5 seconds read timeout
+        tty.c_cflag &= ~CRTSCTS; // no flow control
+        tty.c_lflag = 0; // no signaling chars, no echo, no canonical processing
+        tty.c_oflag = 0; // no remapping, no delays
+        tty.c_cc[VMIN] = 0; // read doesn't block
+        tty.c_cc[VTIME] = 5; // 0.5 seconds read timeout
 
-        tty.c_cflag |= CREAD | CLOCAL;                   // turn on READ & ignore ctrl lines
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY);          // turn off s/w flow ctrl
-        tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);  // make raw
-        tty.c_oflag &= ~OPOST;                           // make raw
+        tty.c_cflag |= CREAD | CLOCAL; // turn on READ & ignore ctrl lines
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+        tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+        tty.c_oflag &= ~OPOST; // make raw
 
         /* Flush Port, then applies attributes */
         // tcflush(fd, TCIOFLUSH);
@@ -219,16 +248,20 @@ bool Connector::connect(Port port) {
     return status;
 }
 
-void Connector::disconnect() throw() {
-    if (!isConnected()) throw std::runtime_error("is not connected");
+void Connector::disconnect() throw()
+{
+    if (!isConnected())
+        throw std::runtime_error("is not connected");
 
     int alpha = close(_fd);
 
     _fd = ~0;
-    if (alpha) throw std::runtime_error(strerror(errno));
+    if (alpha)
+        throw std::runtime_error(strerror(errno));
 }
 
-bool Connector::update() {
+bool Connector::update()
+{
     RequestInitStatus req;
     ResponceInitStatus resp;
 
@@ -242,38 +275,48 @@ bool Connector::update() {
     return alpha;
 }
 
-const DeviceData& Connector::data() const {
-    if (!isConnected()) throw std::runtime_error("Device not connected");
+const DeviceData& Connector::data() const
+{
+    if (!isConnected())
+        throw std::runtime_error("Device not connected");
     return devdata;
 }
 
-ConnectorUtility::ConnectorUtility(Connector& device) : _dev(&device) {}
+ConnectorUtility::ConnectorUtility(Connector& device)
+    : _dev(&device)
+{
+}
 
 const bool ConnectorUtility::connected() const { return (*_dev); }
 
-bool ConnectorUtility::isBacklight() {
+bool ConnectorUtility::isBacklight()
+{
     RequestBacklight rb;
     ResponceBacklight rs;
     _dev->get(rb, rs);
     return rs.mode;
 }
 
-void ConnectorUtility::setBacklight(bool state) {
+void ConnectorUtility::setBacklight(bool state)
+{
     RequestBacklight rb(state);
     _dev->set(rb);
 }
 
-void ConnectorUtility::print(const std::string& text) {
+void ConnectorUtility::print(const std::string& text)
+{
     // TODO: next function
     RequestText rq(text.size());
     bool res = _dev->set(rq, text.c_str());
 }
 
-void ConnectorUtility::setCursorView(bool state) {
+void ConnectorUtility::setCursorView(bool state)
+{
     // TODO: next function
 }
 
-void ConnectorUtility::setCursorPos(int row, int col) {
+void ConnectorUtility::setCursorPos(int row, int col)
+{
     // TODO: next function
 }
 
@@ -281,17 +324,19 @@ int ConnectorUtility::getRows() { return _dev->data().lcdRows; }
 
 int ConnectorUtility::getCols() { return _dev->data().lcdCols; }
 
-void ConnectorUtility::clear() {
+void ConnectorUtility::clear()
+{
     // TODO: next function
 }
 
-connector::dptr* ConnectorUtility::malloc(uint8_t size) {
+connector::dptr* ConnectorUtility::malloc(uint8_t size)
+{
     RequestMalloc req(size);
     ResponceMalloc rem;
     bool result = _dev->get(req, rem);
     return reinterpret_cast<connector::dptr*>(rem.pointer);
 }
 
-void ConnectorUtility::mfree(dptr* memory) {}
+void ConnectorUtility::mfree(dptr* memory) { }
 
 Connector::operator bool() const { return isConnected(); }
